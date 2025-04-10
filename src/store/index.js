5102d6cf51734499d6549516315680aafea80d31
Kamlzeus <yusufova_k@auca.kg>
2025-04-10 12:24:31 +0600

    new

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./slices/todoSlices";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // localStorage
+import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
